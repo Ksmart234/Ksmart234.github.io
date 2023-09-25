c7cef9d392d681f7e4178c428dbe3a63eba7a004
Ksmart234 <100024079+Ksmart234@users.noreply.github.com>
2023-09-25 11:20:07 +0100

    Update main.js

--- main.js
+++ main.js
@@ -29,19 +29,19 @@ let clipNames = [
 let projects = [
   {
     image: 'textures/project.webp',
-    url: 'https://https://github.com/Ksmart234/Client-system-php/tree/main',
+    url: 'https://github.com/Ksmart234/Client-system-php/tree/main',
   },
   {
     image: 'textures/project.jpg',
-    url: 'https://https://github.com/Ksmart234/Guest-pass',
+    url: 'https://github.com/Ksmart234/Guest-pass',
   },
   {
     image: 'textures/project (3).jpg',
-    url: 'https://https://github.com/Ksmart234/Client-system-php/tree/main',
+    url: 'https://github.com/Ksmart234/Client-system-php/tree/main',
   },
   {
     image: 'textures/project (2).jpg',
-    url: 'https://https://github.com/Ksmart234/Guest-pass',
+    url: 'https://github.com/Ksmart234/Guest-pass',
   },
 ];
 let aboutCameraPos = {
